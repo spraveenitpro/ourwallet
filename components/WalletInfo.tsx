b8ee39a13a95a2c7432b8c036d2f1f74a04efe29
Praveen <spraveenitpro@gmail.com>
2025-06-05 13:16:07 -0700

    Add WalletInfo component to display balance

---
+++ components/WalletInfo.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import { WalletInfo as WalletInfoT } from "@breeztech/react-native-breez-sdk-liquid";
+
+export default function WalletInfo({ info }: { info: WalletInfoT }) {
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+      }}
+    >
+      <Text style={{ fontSize: 28, color: "#0184fb" }}>
+        {info.balanceSat} ₿
+      </Text>
+      {info.pendingReceiveSat > 0 && (
+        <Text style={{ color: "#606060", fontStyle: "italic" }}>
+          Pending inward balance: {info.pendingReceiveSat} ₿
+        </Text>
+      )}
+      Add commentMore actions
+      {info.pendingSendSat > 0 && (
+        <Text style={{ color: "#606060", fontStyle: "italic" }}>
+          Pending outward balance: {info.pendingSendSat} ₿
+        </Text>
+      )}
+    </View>
+  );
+}
